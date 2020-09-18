c2bc236ada2bc47214d5bf60687c882f1c4fe570
Paulo Guedes <pauloguedes@m2group.co>
2020-09-18 15:35:54 -0300

    Adiconando popovers e melhorando score

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+$(function () {
+  $('#recompensa1').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('#recompensa2').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('#recompensa3').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('#recompensa4').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+  $('#recompensa5').popover({
+    trigger: 'hover',
+    placement: 'right'
+  });
+})
